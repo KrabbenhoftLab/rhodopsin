b8dfa4dc84d4b9534efd1db141836bfb7ddb1a3c
Katherine Eaton <66837313+kmeaton@users.noreply.github.com>
2020-06-30 15:19:11 -0400

    Add files via upload

---
+++ Tree_pruning_script.R
@@ -0,0 +1,42 @@
+##This script was modified from a script produced by WL Testo and JA Pelosi at the University of Florida
+
+##set working directory
+
+setwd("~/Dropbox/Documents/Ciscoes/FTOL-analysis")
+
+##load geiger and dependencies
+
+library("geiger")
+library("dplyr")
+library("phytools")
+library("ape")
+
+##load tree
+tree<-read.tree("actinopt_12k_treePL.tre")
+tree<-ladderize(tree,right=F)
+
+##check tree
+plot(tree, cex=0.3)
+
+##read in taxon list
+data<-read.table("taxa-used-by-hill-plus-baikal-and-cisco.txt", header = T)
+
+##check data
+data
+
+###check names in list against tree
+name_list<-name.check(tree,data)
+
+##make list of names not in data file
+checked_names<-name_list$tree_not_data
+
+##convert to a vector
+names_to_drop<-as.vector(checked_names)
+
+##trim tree to just taxa represented in list
+trimmed_tree<-drop.tip(tree,names_to_drop)
+
+##visualize trimmed tree
+plot(trimmed_tree, cex=0.35)
+
+write.tree(trimmed_tree)
